14fb9f328112058fa059d1a55f163636e35628d0
schl3sch <schlegelj.tin23@student.dhbw-heidenheim.de>
2026-01-16 22:54:28 +0100

    ich kann nicht mehr ...

--- boards/shields/nice_view/widgets/art.c
+++ boards/shields/nice_view/widgets/art.c
@@ -228,11 +228,11 @@ const lv_img_dsc_t mountain = {
     .data = mountain_map,
 };
 
-#ifndef LV_ATTRIBUTE_IMG_ROCK-LEE
-#define LV_ATTRIBUTE_IMG_ROCK-LEE
+#ifndef LV_ATTRIBUTE_IMG_ROCKLEE
+#define LV_ATTRIBUTE_IMG_ROCKLEE
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_ROCK-LEE uint8_t 
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_ROCKLEE uint8_t 
     rocklee_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
         0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
