04f43e4811a59004d192ca4ab6a0b654f5febae7
schl3sch <schlegelj.tin23@student.dhbw-heidenheim.de>
2026-01-16 21:56:34 +0100

    ich kann nicht mehr ...

--- boards/shields/nice_view_custom/widgets/art.c
+++ boards/shields/nice_view_custom/widgets/art.c
@@ -311,9 +311,8 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_ROCKLEE u
 };
 
 const lv_img_dsc_t rocklee = {
-  .header.cf = LV_IMG_CF_INDEXED_1BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.cf = LV_COLOR_FORMAT_I1,
+  
   .header.w = 105,
   .header.h = 68,
   .data_size = 960,
